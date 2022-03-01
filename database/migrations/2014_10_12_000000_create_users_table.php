213b5b9f175a355d45f7b9ac5f7570db26ed3b32
= <spurkayastha83@gmail.com>
2022-03-01 21:25:33 +0600

    environment setup and  user migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('user_role', ['user', 'admin'])
+                ->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
